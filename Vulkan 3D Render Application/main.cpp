372e784c513a2f54559bb683a9befd4eca0b0840
kora11e <87006301+kora11e@users.noreply.github.com>
2024-03-25 16:53:37 +0100

    Texture data Implementation

--- Vulkan 3D Render Application/main.cpp
+++ Vulkan 3D Render Application/main.cpp
@@ -1,9 +1,6 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
 #define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>

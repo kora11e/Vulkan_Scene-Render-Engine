6d8fe751929ed0c61face13ca99fd5bafa5984a0
kora11e <87006301+kora11e@users.noreply.github.com>
2024-05-27 13:10:32 +0200

    update

--- Vulkan 3D Render Appliacation 3.0/Vulkan 3D Render Application/App.cpp
+++ Vulkan 3D Render Appliacation 3.0/Vulkan 3D Render Application/App.cpp
@@ -1,11 +1,20 @@
 #include "App.h"
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 #include <array>
 
 namespace lve {
+
+	struct PushDataConstant {
+		glm::vec2 offset;
+		alignas(16) glm::vec3 color;
+	};
 
 	App::App() {
 		createPipeline();
@@ -65,12 +74,18 @@ namespace lve {
 	}
 
 	void App::createPipelineLayout() {
+
+		VkPushConstantRange pushConstantRange{};
+		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+		pushConstantRange.offset = 0;
+		pushConstantRange.size = sizeof(PushDataConstant);
+
 		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
 		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
 		pipelineLayoutInfo.setLayoutCount = 0;
 		pipelineLayoutInfo.pSetLayouts = nullptr;
-		pipelineLayoutInfo.pushConstantRangeCount = 0;
-		pipelineLayoutInfo.pPushConstantRanges = nullptr;
+		pipelineLayoutInfo.pushConstantRangeCount = 1;
+		pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 
 		if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
 			throw std::runtime_error("Failed to create Pipeline!");
@@ -98,6 +113,9 @@ namespace lve {
 	}
 
 	void App::recordCommandBuffer(int imageIndex) {
+		static int frame = 0;
+		frame = (frame + 1) & 1000;
+
 		VkCommandBufferBeginInfo beginInfo{};
 		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -134,7 +152,15 @@ namespace lve {
 
 		lvePipeline->bind(commandBuffer[imageIndex]);
 		lveModel->bind(commandBuffer[imageIndex]);
-		lveModel->draw(commandBuffer[imageIndex]);
+
+		for (int i = 0; i < 4; i++) {
+			PushDataConstant push{};
+			push.offset = { -0.5f + frame * 0.002f, -0.4f +i* 0.25f };
+			push.color = { 0.0f, 0.0f, 0.2f + 0.2f * i };
+
+			vkCmdPushConstants(commandBuffer[imageIndex], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(PushDataConstant), &push);
+			lveModel->draw(commandBuffer[imageIndex]);
+		}
 
 		vkCmdDraw(commandBuffer[imageIndex], 3, 1, 0, 0);
 

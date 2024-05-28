a4b0e3d60e83718269e6fd91a392d8ccf174b6fc
kora11e <87006301+kora11e@users.noreply.github.com>
2024-05-28 18:12:11 +0200

    update

--- Vulkan 3D Render Appliacation 3.0/Vulkan 3D Render Application/lve_renderer.cpp
+++ Vulkan 3D Render Appliacation 3.0/Vulkan 3D Render Application/lve_renderer.cpp
@@ -11,52 +11,14 @@
 #include <array>
 
 namespace lve {
-
-	Renderer::Renderer(LveWindow& window, MyEngineDevice& device) {
-		loadgameObjects();
-		createPipelineLayout();
+	
+	Renderer::Renderer(LveWindow& window, MyEngineDevice& device) : lveWindow{ window }, MyEngineDevice{device} {
 		createcommadBuffers();
 		recreateSwapChain();
 	}
 
 	Renderer::~Renderer() {
-		vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr);
-	}
-
-	void Renderer::run() {
-		while (!lveWindow.shouldClose()) {
-
-			glfwPollEvents();
-			drawFrame();
-		}
-
-		vkDeviceWaitIdle(lveDevice.device());
-	}
-
-	void Renderer::loadgameObjects() {
-		std::vector<LveModel::Vertex> vertices{
-			{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-			{{0.5f, 0.5f}, {1.0f, 1.0f, 0.0f}},
-			{{-0.5f, 0.5f}, {1.0f, 0.0f, 1.0f}}
-		};
-		auto lveModel = std::make_shared<LveModel>(lveDevice, vertices);
-
-		auto triangle = LveGameObject::createGameObject();
-		triangle.model = lveModel;
-		triangle.color = { .1f, .8f, .1f };
-		triangle.transform2d.translation.x = .2f;
-		triangle.transform2d.scale = { 2.f, .5f };
-		triangle.transform2d.rotation = .25f * glm::two_pi<float>();
-
-		gameObjects.push_back(std::move(triangle));
-	}
-
-	void Renderer::createPipeline() {
-		PipelineConfigInfo pipelineConfig{};
-		LvePipeline::defaultPipelineConfigInfo(pipelineConfig);
-		pipelineConfig.renderPass = lveSwapChain->getRenderPass();
-		pipelineConfig.layout = pipelineLayout;
-		lvePipeline = std::make_unique<LvePipeline>(lveDevice, "./shader.vert.spv", "./shader.frag.spv", pipelineConfig);
+		freeCommandBuffers();
 	}
 
 	void Renderer::recreateSwapChain() {
@@ -76,26 +38,7 @@ namespace lve {
 				freeCommandBuffers();
 			}
 		}
-		createPipeline();
-	}
-
-	void Renderer::createPipelineLayout() {
-
-		VkPushConstantRange pushConstantRange{};
-		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
-		pushConstantRange.offset = 0;
-		pushConstantRange.size = sizeof(PushDataConstant);
-
-		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
-		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-		pipelineLayoutInfo.setLayoutCount = 0;
-		pipelineLayoutInfo.pSetLayouts = nullptr;
-		pipelineLayoutInfo.pushConstantRangeCount = 1;
-		pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
-
-		if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
-			throw std::runtime_error("Failed to create Pipeline!");
-		}
+		//add stuff later here
 	}
 
 	void Renderer::freeCommandBuffers() {
@@ -162,24 +105,6 @@ namespace lve {
 		}
 	}
 
-	void Renderer::renderGameObjects(VkCommandBuffer commandBuffer) {
-		lvePipeline->bind(commandBuffer);
-
-		for (auto& obj : gameObjects) {
-			obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.01f, glm::two_pi<float>());
-
-			PushDataConstant push{};
-			push.offset = obj.transform2d.translation;
-			push.color = obj.color;
-			push.transform = obj.transform2d.scaleMat2();
-
-
-			vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(PushDataConstant), &push);
-			obj.model->bind(commandBuffer);
-			obj.model->draw(commandBuffer);
-		}
-	};
-
 	void Renderer::drawFrame() {
 		uint32_t imageIndex;
 		auto result = lveSwapChain->acquireNextImage(&imageIndex);
@@ -189,7 +114,7 @@ namespace lve {
 		}
 		if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
 			throw std::runtime_error("Failed to acquire swap chain image!");
-		}
+		} 
 
 		result = lveSwapChain->submitCommandBuffers(&commandBuffer[imageIndex], &imageIndex);
 
